3f031103ee4fcc6600ef1ad7b307391154019ccc
Lavishkamboj <151373968+Lavishkamboj@users.noreply.github.com>
2025-07-21 21:16:23 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
+ // mongoURI
 const connection = mongoose
   .connect(
-     mongoURI)
+    "mongodb+srv://lavishkamboj16:lavishkamboj16...@cluster0.1dmvo.mongodb.net/todoApp?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log(" Connected to MongoDB"))
   .catch((err) => console.error(" MongoDB connection error:", err));
 
