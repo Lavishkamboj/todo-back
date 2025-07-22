948ee5897942d733bd3dd36b7fb62585a72b94c3
Lavishkamboj <151373968+Lavishkamboj@users.noreply.github.com>
2025-07-22 08:38:54 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,21 +1,22 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET="lavish"
+const JWT_SECRET = "lavish";
 
 const requireAuth = (req, res, next) => {
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).send('Access denied. No token provided.');
-  }
-
   try {
-    const decoded = jwt.verify(token,JWT_SECRET);
-    req.user = decoded; // make user info available in req
-   
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).send('Access denied. No token provided.');
+    }
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+
     next();
-    
   } catch (err) {
+    console.error('JWT verification failed:', err);
     return res.status(400).send('Invalid token.');
   }
 };
-module.exports=requireAuth;
+
+module.exports = requireAuth;
